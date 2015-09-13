21e53c21469fc984074c326c6ab3cfb90532233d
fhur <fernando@authy.com>
2015-09-13 21:52:05 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject eaml "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [lexluthor "0.5.0"]
+                 [instaparse "1.4.1"]
+                 [presto "0.2.0"]])
---
+++ test/eaml/core_test.clj
@@ -0,0 +1,7 @@
+(ns eaml.core-test
+  (:require [clojure.test :refer :all]
+            [eaml.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
