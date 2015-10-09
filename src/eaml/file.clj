b6dbb8984f48ccab10d52a2f81d94011fa12791d
fhur <fernando@authy.com>
2015-10-09 07:07:26 +0200

    Added filter-tree and extension-filter functions

--- src/eaml/file.clj
+++ src/eaml/file.clj
@@ -1,5 +1,6 @@
 (ns eaml.file
-  (:require [clojure.java.io :refer :all]))
+  (:require [clojure.java.io :refer :all])
+  (:import [java.io File]))
 
 (defn read-file
   "Reads the file located at file-path and returns the contents as a string"
@@ -11,3 +12,32 @@
         result
         (recur (str result line "\n")
                (.readLine reader))))))
+
+
+(defn extension-filter
+  "Creates an anonymous function which takes a file as argument and returns
+  true if the file has the given extension.
+  Example: (extension-filter \"clj\") will match clojure files."
+  [ext]
+  (fn [file]
+    (let [file-name (.getName file)
+          match (re-find #".+\.(.+)" file-name)]
+      (if (nil? match)
+        false
+        (= ext (last match))))))
+
+
+(defn filter-tree
+  "Reads a file tree recursively and returns a list of files that match the
+  given filter function f. To match by extension see the extension-filter
+  function."
+  [root-dir-path f]
+  (loop [files [(File. root-dir-path)]
+         result []]
+    (if (empty? files)
+      result
+      (let [file (first files)]
+        (if (.isDirectory file)
+          (recur (into (rest files) (.listFiles file)) result)
+          (recur (rest files) (if (f file) (conj result file) result)))))))
+
