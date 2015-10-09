b514aed461367e7d06c900d42c48163e903a4db3
fhur <fernando@authy.com>
2015-10-09 07:09:43 +0200

    Added parse-dir to parser

--- src/eaml/parser.clj
+++ src/eaml/parser.clj
@@ -1,6 +1,13 @@
 (ns eaml.parser
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta]
+            [eaml.file :refer :all]))
 
 (def parser (insta/parser (clojure.java.io/resource "grammar.bnf")
                           :auto-whitespace :standard))
 
+(defn parse-dir
+  [root-path]
+  (->> (filter-tree root-path (extension-filter "eaml"))
+       (map read-file)
+       (map parser)
+       (reduce conj)))
