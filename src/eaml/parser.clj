09ce7abe66a768cc7d5708f564a788da5125bb0c
fhur <fernando@authy.com>
2015-10-14 00:35:02 +0200

    Added tests to parser and verified basic parsing functionality
    Parsing functionality tested so far:
    - colors
    - dimens
    - styles
      - multiple parents
      - defining attrs as both pointers and literals
      - support for string literals
      - support for native literals
      - support for color literals
      - support for dimen literals

--- src/eaml/parser.clj
+++ src/eaml/parser.clj
@@ -1,6 +1,8 @@
 (ns eaml.parser
   (:require [instaparse.core :as insta]
-            [eaml.file :refer :all]))
+            [eaml.file :refer :all]
+            [eaml.util :refer :all]
+            [eaml.error :refer :all]))
 
 (def parser (insta/parser (clojure.java.io/resource "grammar.bnf")
                           :auto-whitespace :standard))
@@ -11,3 +13,79 @@
        (map read-file)
        (map parser)
        (reduce conj)))
+
+
+(defn parse-attr-value
+  [attr]
+  (let [[attr-type attr-value] attr]
+    (vector (cond (#{:color-literal :dimen-literal
+                     :native-pointer :string-literal} attr-type)
+                    :literal
+                  (#{:pointer} attr-type)
+                    :pointer
+                  :else (raise! "Unknown attr type " attr-type " in " attr))
+            (cond (= :string-literal attr-type)
+                    (.replaceAll attr-value "\"" "")
+                  :else
+                    attr-value))))
+
+
+(defn parse-color-or-dimen
+  [ast-node]
+  (let [[_ node-type id attr] ast-node]
+    {:node (keyword node-type)
+     :id id
+     :value (parse-attr-value attr)}))
+
+
+(defn parse-parents
+  [ast-node]
+  (rest (find-first (fn [node]
+                      (and (coll? node) (= :extends-expr (first node))))
+                    ast-node [])))
+
+[:attr-def "android:background" [:attr-value [:string-literal "\"@drawable/foo_drawable\""]]]
+(defn parse-attr
+  ([attr config]
+   (if (= (first attr) :attr-def)
+     (let [[_ attr-name [_ attr-value]] attr]
+       {:name attr-name
+        :value (parse-attr-value attr-value)
+        :config config})
+     (raise! "Unknown attr " attr)))
+  ([attr]
+   (parse-attr attr :default)))
+
+
+
+(defn parse-attrs
+  [ast-node]
+  (->> (find-first #(and (coll? %) (= :attrs (first %))) ast-node [])
+       (rest)
+       (map parse-attr)))
+
+
+
+(defn parse-style
+  [ast-node]
+  (let [[_ _ id & rst] ast-node]
+    {:node :style
+     :id id
+     :parents (parse-parents ast-node)
+     :attrs (parse-attrs ast-node)}))
+
+
+(defn normalize-node
+  [ast-node]
+  (let [[_ ast-node-type id & other] ast-node]
+    (cond (= ast-node-type "style")
+            (parse-style ast-node)
+          (#{"color" "dimen"} ast-node-type)
+            (parse-color-or-dimen ast-node)
+          :else
+            (raise! "Unrecognized node type: '" ast-node-type "' in " ast-node))))
+
+(defn normalize-nodes
+  [ast-nodes]
+  (for [ast-node ast-nodes]
+    (normalize-node ast-node)))
---
+++ test/eaml/parser_test.clj
@@ -0,0 +1,76 @@
+(ns eaml.parser-test
+  (:require [eaml.parser :refer :all]
+            [presto.core :refer :all]
+            [clojure.test :refer :all]))
+
+(defn parse-first
+  [string]
+  (->> (parser string)
+       (first)
+       (normalize-node)))
+
+(expected-when "parsing colors" parse-first
+  when ["color red: #ff0000;"]
+     = {:id "red"
+        :node :color
+        :value [:literal "#ff0000"]}
+
+  when ["color some_other_color: #f12;"]
+     = {:id "some_other_color"
+        :node :color
+        :value [:literal "#f12"]})
+
+(expected-when "parsing dimens" parse-first
+  when ["dimen small_margins: 12dp;"]
+     = {:id "small_margins"
+        :node :dimen
+        :value [:literal "12dp"]}
+
+  when ["dimen large_text: 12sp;"]
+     = {:id "large_text"
+        :node :dimen
+        :value [:literal "12sp"]})
+
+(expected-when "parsing styles" parse-first
+  when ["style Foo {}"]
+     = {:id "Foo"
+        :node :style
+        :parents []
+        :attrs []}
+
+  when ["style FooBar < Foo {}"]
+     = {:id "FooBar"
+        :node :style
+        :parents ["Foo"]
+        :attrs []}
+
+  when ["style FooBar < Boo, Foo {}"]
+     = {:id "FooBar"
+        :node :style
+        :parents ["Boo" "Foo"]
+        :attrs []}
+
+  when ["style FooBar123 < Foo, Bar, Baz {
+           android:textColor: #ff0000;
+           android:textSize: 12sp;
+         }"]
+     = {:id "FooBar123"
+        :node :style
+        :parents ["Foo" "Bar" "Baz"]
+        :attrs [{:name "android:textColor" :value [:literal "#ff0000"] :config :default}
+                {:name "android:textSize" :value [:literal "12sp"] :config :default}]}
+
+  when ["style Foo < Bar {
+          android:textColor: #123;
+          android:background: @drawable/foo_drawable;
+          android:textSize: small_text;
+          android:text: \"some text\";
+        }"]
+     = {:id "Foo"
+        :node :style
+        :parents ["Bar"]
+        :attrs [{:name "android:textColor" :value [:literal "#123"] :config :default}
+                {:name "android:background" :value [:literal "@drawable/foo_drawable"] :config :default}
+                {:name "android:textSize" :value [:pointer "small_text"] :config :default}
+                {:name "android:text" :value [:literal "some text"] :config :default}]})
+
