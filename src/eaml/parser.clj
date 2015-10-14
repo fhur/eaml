b9cb8870deaf8f1bf210904a9f9c889bbf4d490a
fhur <fernando@authy.com>
2015-10-14 22:19:45 +0200

    Minor cleanup in eaml.parser, changed visibility for parser helper functions

--- src/eaml/parser.clj
+++ src/eaml/parser.clj
@@ -4,18 +4,8 @@
             [eaml.util :refer :all]
             [eaml.error :refer :all]))
 
-(def parser (insta/parser (clojure.java.io/resource "grammar.bnf")
-                          :auto-whitespace :standard))
 
-(defn parse-dir
-  [root-path]
-  (->> (filter-tree root-path (extension-filter "eaml"))
-       (map read-file)
-       (map parser)
-       (reduce conj)))
-
-
-(defn parse-attr-value
+(defn- parse-attr-value
   [attr]
   (let [[attr-type attr-value] attr]
     (vector (cond (#{:color-literal :dimen-literal
@@ -30,7 +20,7 @@
                     attr-value))))
 
 
-(defn parse-color-or-dimen
+(defn- parse-color-or-dimen
   [ast-node]
   (let [[_ node-type id attr] ast-node]
     {:node (keyword node-type)
@@ -38,14 +28,13 @@
      :value (parse-attr-value attr)}))
 
 
-(defn parse-parents
+(defn- parse-parents
   [ast-node]
   (rest (find-first (fn [node]
                       (and (coll? node) (= :extends-expr (first node))))
                     ast-node [])))
 
-[:attr-def "android:background" [:attr-value [:string-literal "\"@drawable/foo_drawable\""]]]
-(defn parse-attr
+(defn- parse-attr
   ([attr config]
    (if (= (first attr) :attr-def)
      (let [[_ attr-name [_ attr-value]] attr]
@@ -58,7 +47,7 @@
 
 
 
-(defn parse-attrs
+(defn- parse-attrs
   [ast-node]
   (->> (find-first #(and (coll? %) (= :attrs (first %))) ast-node [])
        (rest)
@@ -66,13 +55,15 @@
 
 
 
-(defn parse-style
+(defn- parse-style
   [ast-node]
   (let [[_ _ id & rst] ast-node]
     {:node :style
      :id id
      :parents (parse-parents ast-node)
      :attrs (parse-attrs ast-node)}))
+
+
 
 
 (defn normalize-node
@@ -85,7 +76,21 @@
           :else
             (raise! "Unrecognized node type: '" ast-node-type "' in " ast-node))))
 
+
 (defn normalize-nodes
   [ast-nodes]
   (for [ast-node ast-nodes]
     (normalize-node ast-node)))
+
+
+(def parser (insta/parser (clojure.java.io/resource "grammar.bnf")
+                          :auto-whitespace :standard))
+
+(defn parse-dir
+  [root-path]
+  (->> (filter-tree root-path (extension-filter "eaml"))
+       (map read-file)
+       (map parser)
+       (reduce conj)))
+
+
