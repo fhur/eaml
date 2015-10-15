daa07cd64613f1fc47344b53f9095863d8d67bd2
fhur <fernando@authy.com>
2015-10-16 00:04:06 +0200

    Added better error messages for eaml.error/raise

--- src/eaml/error.clj
+++ src/eaml/error.clj
@@ -1,7 +1,14 @@
-(ns eaml.error)
+(ns eaml.error
+  (:require [clojure.pprint :refer [pprint]]
+            [clojure.string :refer [join]]))
+
+(defn- ppr
+  [obj]
+  (with-out-str (clojure.pprint/pprint obj)))
 
 (defn raise!
   "Throws a RuntimeException with the given message as argument"
   [& messages]
-  (throw (new RuntimeException (apply str messages))))
+  (throw (new RuntimeException (join "" (map ppr messages)))))
+
 
