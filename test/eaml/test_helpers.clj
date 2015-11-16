98c9f750b199e76cfac5c93b627744c8e50b2be2
Fernando <fernandohur@gmail.com>
2015-11-16 22:26:43 +0100

    Created test helper functions

---
+++ test/eaml/test_helpers.clj
@@ -0,0 +1,42 @@
+(ns eaml.test-helpers
+  "Place common helper functions for testing"
+  (:require [clojure.string :refer [split-lines join]]))
+
+(defn cleanup-xml
+  [xml-string]
+  (->> (split-lines xml-string)
+       (map #(.trim %))
+       (join)))
+
+
+(defn xml=?
+  "Tests that two xml strings are semantically equal.
+  This does not include new lines, indentation, etc."
+  [expected actual]
+  (= (cleanup-xml expected)
+     (cleanup-xml actual)))
+
+(defmacro simpleres
+  [type name value]
+  [(keyword type) {:name name} value])
+
+(defmacro resources
+  [& nodes]
+  (vec (cons :resources nodes)))
+
+(defmacro color
+  [name value]
+  (simpleres :color name value))
+
+(defmacro dimen
+  [name value]
+  (simpleres :dimen name value))
+
+(defmacro string
+  [name value]
+  (simpleres :string name value))
+
+(defmacro bool
+  [name value]
+  (simpleres :bool name value))
+
