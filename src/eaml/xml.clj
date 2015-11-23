65aacfbe42b5463d855165ec4f62bc31f402cf32
Fernando <fernandohur@gmail.com>
2015-11-23 08:28:15 +0100

    Rewrote xml namespace using a much cleaner data->xml based approach that is completely decoupled

--- src/eaml/xml.clj
+++ src/eaml/xml.clj
@@ -4,58 +4,36 @@
             [eaml.file :refer [path-concat mkdirs!]])
   (:import [java.io StringWriter]))
 
-(defn- render-items
-  [items]
-  (map (fn [{name :name value :value}]
-         (str "<item name=\"" name "\">"
-              value
-              "</item>"))
-       items))
-
-(defn render-style-node
-  "Expects a structure with the following form:
-   {:name <style name>
-    :item [{:name <attr name> :value <attr value>}, ... ]}
-  returns the given style node as an xml string"
-  [style-name items parent]
-  (str "<style name=\"" style-name "\""
-       (if (nil? parent) "" (str " parent=\"" parent "\""))
-       ">"
-       (->> (render-items items)
-            (join "\n"))
-       "</style>"))
-
-(defn write-to-writer!
-  [nodes writer]
-  ;; Initialize the writer
-  (with-open [writer writer]
-
-    ;; Write the header
-    (.write writer "<resources>\n")
-
-    ;; Write the body of the file by converting each
-    ;; node to its xml representation
-    (doseq [{name :id attrs :attrs parent :parent} nodes]
-      (.write writer (render-style-node name attrs parent)))
-
-    ;; Write the footer
-    (.write writer "\n</resources>")))
-
-(defn write-str
-  [nodes]
-  (let [writer (StringWriter. )]
-    (write-to-writer! nodes writer)
-    (.toString writer)))
+(declare render-xml-node-start
+         render-xml-node-body
+         render-xml-node-end)
 
 (defn string-writer
   "Initializes a new StringWriter"
   [] (StringWriter. ))
 
-(declare render-xml-node-start
-         render-xml-node-body
-         render-xml-node-end)
-
 (defn render-xml
+  "Writes a tree of nodes to the given writer.
+  node must be in the following form:
+  [:node {:arg1 'val1' :arg2 'val2' ...}
+   [:subnode1 {:arg1 'val1'} ...]
+   [:subnode2 {:arg1 'val1'} ...]
+   ...]
+
+  Note: this function does not handle closing of the writer.
+  You must do this yourself.
+
+  Example:
+  [:resources {}
+    [:style {name 'Button' parent 'BaseButton'}
+     [:item {name 'android:textSize'} '12sp']]]
+
+  Will write
+  <resources>
+    <style name='Button' parent='BaseButton'>
+      <item name='android:textSize'>12sp</item>
+    </style>
+  </resources>"
   [node writer]
   (let [writer writer]
     (loop [node-queue (list node)]
@@ -67,6 +45,15 @@
           (render-xml-node-body head writer)
           (render-xml-node-end head writer)
           (recur tail))))))
+
+(defn render-xml-string
+  "Writes a tree of nodes to a string and returns that string.
+  Use this method only for debbuging.
+  Equivalent to render-xml using string-writer as a writer"
+  [nodes]
+  (let [writer (string-writer)]
+    (render-xml nodes writer)
+    (.toString writer)))
 
 (defn- render-xml-node-start
   [[node-type attrs & subnodes] writer]
@@ -93,8 +80,3 @@
   (.write writer (name node-type))
   (.write writer ">"))
 
-(defn render-xml-string
-  [nodes]
-  (let [writer (string-writer)]
-    (render-xml nodes writer)
-    (.toString writer)))
