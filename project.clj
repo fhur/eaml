aefa870cdc5f3935080345ef0dbba518e94eca0c
Fernando <fernandohur@gmail.com>
2015-11-30 15:06:15 -0500

    Added cloverage plugin

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :url "http://github.com/fhur/eaml"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cloverage "1.0.6"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [instaparse "1.4.1"]
                  [presto "0.2.0"]])
